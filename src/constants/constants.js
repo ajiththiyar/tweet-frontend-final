c0ea3b4d52fe3da2cb32af7f39362887291ded0b
ajith thiyar <ajiththiyar@gmail.com>
2022-08-11 12:18:17 +0530

    Initial Commit

---
+++ src/constants/constants.js
@@ -0,0 +1,1 @@
+export const URI = "http://tweetbackend-env.eba-gbtjfd8x.ap-south-1.elasticbeanstalk.com";
---
+++ src/store/auth-context.js
@@ -0,0 +1,77 @@
+import React, { useCallback, useEffect, useState } from "react";
+
+const AuthContext = React.createContext({
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+  user: "",
+});
+
+
+const getTokenFromLocal = () => {
+  const initialToken = localStorage.getItem("token");
+  const user = JSON.parse(localStorage.getItem("user"));
+
+ console.log({ token: initialToken });
+
+  return { token: initialToken,  user };
+};
+
+export const AuthContextProvider = (props) => {
+  const storedToken = getTokenFromLocal();
+  let initialToken, initialUser;
+  if (storedToken) {
+    initialToken = storedToken.token;
+    initialUser = storedToken.user;
+  }
+  const [token, setToken] = useState(initialToken);
+  const [user, setUser] = useState(initialUser);
+
+  const userIsLoggedIn = !!token;
+
+  const logoutHandler = useCallback(() => {
+    console.log("logOut");
+    setToken(null);
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+
+  }, []);
+  const loginHandler = (token, userDetails) => {
+
+    console.log("login");
+    setToken(token);
+    setUser(userDetails);
+
+    localStorage.setItem("token", token);
+    localStorage.setItem("user", JSON.stringify(userDetails));
+
+  };
+
+
+  console.log({
+    token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  });
+  return (
+    <AuthContext.Provider
+      value={{
+        token,
+        isLoggedIn: userIsLoggedIn,
+        login: loginHandler,
+        logout: logoutHandler,
+        user,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return React.useContext(AuthContext);
+};
+
+export default AuthContext;
